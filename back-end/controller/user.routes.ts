a248550b56076aaec88f21c231394febb935e265
Ammani Ali Khan <r1006227@ucll.be>
2024-12-20 11:44:10 +0100

    swagger added

--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -1,3 +1,39 @@
+/**
+ * @swagger
+ *   components:
+ *    securitySchemes:
+ *     bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ *    schemas:
+ *      User:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: number
+ *              description: User ID.
+ *            username:
+ *              type: string
+ *              description: Unique username of the user.
+ *            password:
+ *              type: string
+ *              description: Encrypted user password.
+ *            email:
+ *              type: string
+ *              description: Email of the user.
+ *            bio:
+ *              type: string
+ *              description: User's biography.
+ *            age:
+ *              type: number
+ *              description: User's age.
+ *            creationDate:
+ *              type: string
+ *              format: date-time
+ *              description: Date when the user account was created.
+ */
+
 import express, { NextFunction, Request, Response } from 'express';
 import userService from '../service/user.service';
 import { UserInput } from '../types';
@@ -5,10 +41,26 @@ import { User } from '@prisma/client';
 
 const userRouter = express.Router();
 
-
-
-
-userRouter.get('/all', async (req: Request , res: Response) => {
+/**
+ * @swagger
+ * /users/all:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get a list of all users.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error.
+ */
+userRouter.get('/all', async (req: Request, res: Response) => {
     try {
         const users = await userService.getAllUsers();
         res.status(200).json(users);
@@ -18,34 +70,148 @@ userRouter.get('/all', async (req: Request , res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /users/{username}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get a user by username.
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user.
+ *     responses:
+ *       200:
+ *         description: User object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 userRouter.get('/:username', async (req, res, next: NextFunction) => {
     try {
-        const username = req.params.username; 
-        const user = await userService.getUserByUsername(username)
-        
-        res.status(200).json(user);
+        const username = req.params.username;
+        const user = await userService.getUserByUsername(username);
 
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(user);
     } catch (error) {
         next(error);
-
-    };
+    }
 });
 
+/**
+ * @swagger
+ * /users/{username}:
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Delete a user by username.
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user to delete.
+ *     responses:
+ *       200:
+ *         description: User deleted successfully.
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 userRouter.delete('/:username', async (req, res, next: NextFunction) => {
-    const username = req.params.username;
-    const user = await userService.deleteUserByUsername(username);
-    res.status(200).json(user);
-})
+    try {
+        const username = req.params.username;
+        const user = await userService.deleteUserByUsername(username);
 
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * @swagger
+ * /users/{username}/{bio}:
+ *   put:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Update a user's bio by username.
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username of the user to update.
+ *       - in: path
+ *         name: bio
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: New bio for the user.
+ *     responses:
+ *       200:
+ *         description: User bio updated successfully.
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 userRouter.put('/:username/:bio', async (req, res, next: NextFunction) => {
-    const username = req.params.username;
-    const bio = req.params.bio;
+    try {
+        const username = req.params.username;
+        const bio = req.params.bio;
 
-    const user = await userService.updateUserByUsername(username, bio);
-    res.status(200).json(user);
-})
+        const user = await userService.updateUserByUsername(username, bio);
 
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
 
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * @swagger
+ * /users/signup:
+ *   post:
+ *     summary: Create a new user (sign up).
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User created successfully.
+ *       400:
+ *         description: Invalid input data.
+ *       500:
+ *         description: Internal server error.
+ */
 userRouter.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = <UserInput>req.body;
@@ -56,6 +222,35 @@ userRouter.post('/signup', async (req: Request, res: Response, next: NextFunctio
     }
 });
 
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Authenticate a user (login).
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The username of the user.
+ *               password:
+ *                 type: string
+ *                 description: The password of the user.
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Authentication successful, returns a JWT token.
+ *       401:
+ *         description: Invalid credentials.
+ *       500:
+ *         description: Internal server error.
+ */
 userRouter.post('/login', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = <UserInput>req.body;
@@ -66,9 +261,4 @@ userRouter.post('/login', async (req: Request, res: Response, next: NextFunction
     }
 });
 
-
-
-
-export {userRouter};
-
-
+export { userRouter };
